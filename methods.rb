4c08079be6e9871a7e4b14d2b4875af9853f1f29
osteen <osteen.mutinda@student.moringaschool.com>
2023-02-07 13:13:02 +0300

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+ 
+def greet(name = "Jimmy")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+def add(x, y)
+    x + y
+end
+  
+def halve(b)
+    b / 2
+  end
+ 
+def halve(b)
+   return nil unless b.to_i == b
+    b / 2
+end
+    
